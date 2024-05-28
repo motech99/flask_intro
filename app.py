2c85b401c1130ea91e10efc3e99bbde15b4ef375
Mohammed Hani <104127533+motech99@users.noreply.github.com>
2024-05-28 17:39:01 +1000

    completed lesson

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ def hello_world():
     return"<p>Hello, World!</p>"
 
 
+@app.route('/goodbye/')
+def goodbye_world():
+    return "<p>Goodbye, World!</p>"
+
+@app.route("/coder/")
+def coder():
+    return "<p>This  web app was created in a class at Coder Academy.</p>"
